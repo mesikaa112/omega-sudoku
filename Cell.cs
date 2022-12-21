8db94f66da20caf3529f8755ccb6f0c419cf75b9
Yonatan Mesika <yonatan11mesika@gmail.com>
2022-12-21 18:00:20 +0200

    added Cell class with value of the cell

---
+++ Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku
+{
+    internal class Cell
+    {
+        private int value;
+
+        public Cell(int value)
+        {
+            // Initialize the cell with a value
+            this.value = value;
+        }
+
+        public int GetValue()
+        {
+            return value;
+        }
+
+        public void SetValue(int newValue)
+        {
+            value = newValue;
+        }
+    }
+}
