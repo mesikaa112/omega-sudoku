1c2ba7f0fe99c17e16736819a4cf7e6d48454499
Yonatan Mesika <yonatan11mesika@gmail.com>
2022-12-23 13:35:44 +0200

    added inheritance from Exception

--- TwoValuesInTheSameSubSquereError.cs
+++ TwoValuesInTheSameSubSquereError.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OmegaSudoku
 {
-    internal class TwoValuesInTheSameSubSquereError
+    internal class TwoValuesInTheSameSubSquereError : Exception
     {
         public TwoValuesInTheSameSubSquereError(string message)
         {
