4a5766103ad547c40e51af4dea21cfc4644f84f7
Yonatan Mesika <yonatan11mesika@gmail.com>
2022-12-23 13:39:12 +0200

    added check validation for the row, col and sub squeres for the string before starting to solve

---
+++ SudokuValidation.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku
+{
+    internal class SudokuValidation
+    {
+        public Board CheckValidate(string boardString)
+        {
+            // if the length of the string is not equals to rows*cols
+            if (boardString.Length != Constants.ROWS * Constants.COLS)
+            {
+                throw new InvalidBoardLengthException("there is an Error! you enterd invalid amount of cells");
+            }
+            // create board object
+            Board board = new Board(boardString);
+            if (!CheckInRowValidate(board))
+                throw new TwoValuesInTheSameRowError("there is an Error! there is two same values in the same row");
+            if (!CheckInColValidate(board))
+                throw new TwoValuesInTheSameColError("there is an Error! there is two same values in the same col");
+            if (!CheckInSubSqueresValidate(board))
+                throw new TwoValuesInTheSameSubSquereError("there is an Error! there is two same values in the same col");
+            return board;
+        }
+
+
+        public bool CheckInRowValidate(Board board)
+        {
+            // this method gets the baord, if there is 2 same numbers in the same row the method returns true, false otherwise
+            
+            for (int i = 0; i < Constants.ROWS; i++)
+            {
+                // copy the ellemnts of ROWCOLVALUES to a new array called temp
+                int[] temp = new int[Constants.ROWCOLVALUES.Length];
+                Constants.ROWCOLVALUES.CopyTo(temp, 0);
+
+                for (int j = 0; j < Constants.COLS; j++)
+                {
+                    // if temp contains the board[i, j], the cell in temp will be 0
+                    if (temp.Contains(board.GetBoard()[i, j].GetValue()))
+                    {
+                        temp[board.GetBoard()[i, j].GetValue() - 1] = 0;
+                    }
+                    // if not, it means that there is a value twice or more in the same row or the number is not between 1 to ROWS
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        public bool CheckInColValidate(Board board)
+        {
+            // this method gets the baord, if there is 2 same numbers in the same col the method returns true, false otherwise
+            
+            for (int j = 0; j < Constants.COLS; j++)
+            {
+                // copy the ellemnts of ROWCOLVALUES to a new array called temp
+                int[] temp = new int[Constants.ROWCOLVALUES.Length];
+                Constants.ROWCOLVALUES.CopyTo(temp, 0);
+
+                for (int i = 0; i < Constants.ROWS; i++)
+                {
+                    // if temp contains the board[i, j], the cell in temp will be 0
+                    if (temp.Contains(board.GetBoard()[i, j].GetValue()))
+                    {
+                        temp[board.GetBoard()[i, j].GetValue() - 1] = 0;
+                    }
+                    // if not, it means that there is a value twice or more in the same col or the number is not between 1 to ROWS
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static bool CheckInSubSqueresValidate(Board board)
+        {
+            // this method gets the baord, if there is 2 same numbers in the same sub squere the method returns true, false otherwise
+
+            // squre root of ROWS and COLS
+            int sqrtRow = (int)Math.Sqrt(Constants.ROWS);
+            int sqrtCol = (int)Math.Sqrt(Constants.COLS);
+
+            for (int i = 0; i < Constants.ROWS; i += sqrtRow)
+            {
+                for (int j = 0; j < Constants.COLS; j += sqrtCol)
+                {
+                    // copy the ellemnts of box to a new matrix called temp
+                    int[] temp = new int[Constants.ROWCOLVALUES.Length];
+                    Constants.ROWCOLVALUES.CopyTo(temp, 0);
+                    
+                    // loop on the sub squeres in the board
+                    for (int row = i; row < i + sqrtRow; row++)
+                    {
+                        for (int col = j; col < j + sqrtCol; col++)
+                        {
+                            // if temp contains the board[i, j], the cell in temp will be 0
+                            if (temp.Contains(board.GetBoard()[row, col].GetValue()))
+                            {
+                                temp[board.GetBoard()[row, col].GetValue() - 1] = 0;
+                            }
+                            // if not, it means that there is a value twice or more in the same sub squere
+                            else
+                                return false;
+                        }
+                    }
+
+                }
+            }
+            return true;
+        }
+    }
+}
