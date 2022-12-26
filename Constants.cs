977021af06c84ba9f2de34619cc759e579a0dfff
Yonatan Mesika <yonatan11mesika@gmail.com>
2022-12-26 13:25:14 +0200

    added Crook's algorithm. Crook's algorithm is an algorithm that loops on every cell in the board and give it the possible values that can be in there

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku
+{
+    class Constants
+    {
+        public const int ROWS = 9;
+        public const int COLS = 9;
+        public static int[] ROWCOLVALUES = Enumerable.Range(1, Constants.ROWS).ToArray();
+ 
+    }
+}
