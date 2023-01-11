418de6d8c15114cd4c3828f74e0533a1cc4b7566
Yonatan Mesika <yonatan11mesika@gmail.com>
2023-01-11 15:05:34 +0200

    change ROWS and ROWS to SIZE

--- Constants.cs
+++ Constants.cs
@@ -9,34 +9,43 @@ namespace OmegaSudoku
 {
     class Constants
     {
-        public static int ROWS;
-        public static int COLS;
+        // The number of constraints there are for each cell in the board
+        public const int NUMBER_OF_CONSTRAINTS = 4;
+
+        public static int SIZE;
         // public static int[] ROWCOLVALUES = Enumerable.Range(1, 9).ToArray();
-        public static char[] VALUESOFCELLS;
-        public static System.Collections.ArrayList VALIDCHARACTERS = new ArrayList();
-        
+        public static char[] VALUES_OF_CELLS;
+        public static System.Collections.ArrayList VALID_CHARACTERS = new ArrayList();
+
+        /// <summary>
+        /// thid method added an array list of all the valid characters that can be in the input
+        /// </summary>
+        /// <returns> array list of all the valid characters that can be in the input </returns>
         public static ArrayList AddValidCharacters()
         {
-            for (int i = 0; i <= ROWS; i++)
+            for (int i = 0; i <= SIZE; i++)
             {
                 char possibleValue = (char)i;
                 possibleValue += '0';
-                VALIDCHARACTERS.Add(possibleValue);
+                VALID_CHARACTERS.Add(possibleValue);
             }
-            return VALIDCHARACTERS;
+            return VALID_CHARACTERS;
         }
 
-
+        /// <summary>
+        /// this method added an array of char with values that can be in a cell
+        /// </summary>
+        /// <returns></returns>
         public static char[] AddValuesOfCells()
         {
-            VALUESOFCELLS = new char[ROWS];
-            for (int i = 1; i <= ROWS; i++)
+            VALUES_OF_CELLS = new char[SIZE];
+            for (int i = 1; i <= SIZE; i++)
             {
                 char value = (char)i;
                 value += '0';
-                VALUESOFCELLS[i - 1] = value;
+                VALUES_OF_CELLS[i - 1] = value;
             }
-            return VALUESOFCELLS;
+            return VALUES_OF_CELLS;
         }
     }
 }
