4a1d896f5cbc7f67362a2dadc249360fbbb9091e
Yonatan Mesika <yonatan11mesika@gmail.com>
2023-01-11 15:03:24 +0200

    added more comments and erase check validation for one cell

--- SudokuValidation.cs
+++ SudokuValidation.cs
@@ -4,63 +4,40 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OmegaSudoku.Exceptions;
 
 namespace OmegaSudoku
 {
     internal class SudokuValidation
     {
-        public static bool IsValidPlacement(Board board, int row, int col, int value)
+        /// <summary>
+        /// this method checks the validate of the board before starting solve it
+        /// </summary>
+        /// <param name="boardString"> the board in string type </param>
+        /// <returns> if the board is validate, return the board in int[,] type </returns>
+        /// <exception cref="InvalidBoardLengthError"> there are invalid amount of characters in the board string </exception>
+        /// <exception cref="InvalidCharactersInTheStringError"> there are invalid characters that can't be in a cell </exception>
+        /// <exception cref="TwoValuesInTheSameRowError"> there are two or more same values in the same row </exception>
+        /// <exception cref="TwoValuesInTheSameColError"> there are two or more same values in the same column </exception>
+        /// <exception cref="TwoValuesInTheSameSubSquareError"> there are two or more same values in the same sub square </exception>
+        public static MatrixIntBoard CheckValidate(string boardString)
         {
-            // this method checks if a value can be placed in the board in the row, col placement
-
-            // Check if the value appears in the same row or column
-            for (int i = 0; i < Constants.ROWS; i++)
-            {
-                if (board.GetBoard()[row, i].GetValue() == value || board.GetBoard()[i, col].GetValue() == value) 
-                    return false;
-            }
-
-            // check if the value appears in the same sub square
-            // squre root of ROWS and COLS
-            int sqrtRow = (int)Math.Sqrt(Constants.ROWS);
-            int sqrtCol = (int)Math.Sqrt(Constants.COLS);
-
-            int startRow = (row / sqrtRow) * sqrtRow;
-            int startCol = (col / sqrtCol) * sqrtCol;
-            for (int i = startRow; i < startRow + sqrtRow; i++)
-            {
-                for (int j = startCol; j < startCol + sqrtCol; j++)
-                {
-                    if (board.GetBoard()[i, j].GetValue() == value) 
-                        return false;
-                }
-            }
-            return true;
-        }
-
-
-        public static Board CheckValidate(string boardString)
-        {
-            // this method check the validate of the board before starting calculate it
-
             // if the length have an integer sqrt, the length of the string is valid
             // and if the length of the string is valid, set the amount of rols and cols to this sqrt
-            double sqrtRow = Math.Sqrt(boardString.Length);
-            double sqrtCol = Math.Sqrt(boardString.Length);
-            if (sqrtRow != (int)sqrtCol)
+            double sqrtSize = Math.Sqrt(boardString.Length);
+            if (sqrtSize != (int)sqrtSize)
             {
                 throw new InvalidBoardLengthError("there is an Error! you enterd invalid amount of cells");
             }
             else
             {
-                Constants.ROWS = (int)sqrtRow;
-                Constants.COLS = (int)sqrtCol;
+                Constants.SIZE = (int)sqrtSize;
             }
             // if there are invalid characters in the string, raise Error
             if (!CheckInvalidCharacters(boardString))
                 throw new InvalidCharactersInTheStringError("there is an Error! you entered invalid characters");
             // create board object
-            Board board = new Board(boardString);
+            MatrixIntBoard board = new MatrixIntBoard(boardString);
             // check validate of Rows before stating to solve the sudoku
             if (!CheckInRowValidate(board))
                 throw new TwoValuesInTheSameRowError("there is an Error! there is two same values in the same row");
@@ -73,48 +50,53 @@ namespace OmegaSudoku
             return board;
         }
 
-
-
+        /// <summary>
+        /// this method gets the board in string type and check if there are no invalid characters in it. 
+        /// if there are, the method return false, and true otherwise
+        /// </summary>
+        /// <param name="boardString"> the board in string type </param>
+        /// <returns> false if there are invalid characters in the board, and true otherwise </returns>
         public static bool CheckInvalidCharacters(string boardString)
         {
-            // this method get the string of the baord and check if there are no invakid characters in it.
-            // if there are, the method return false, and true otherwise
-
-            Constants.VALIDCHARACTERS = Constants.AddValidCharacters();
+            Constants.VALID_CHARACTERS = Constants.AddValidCharacters();
             // loop on every character in the string and check if he is valid
-            foreach(char c in boardString)
+            foreach (char c in boardString)
             {
-                if (!Constants.VALIDCHARACTERS.Contains(c))
+                if (!Constants.VALID_CHARACTERS.Contains(c))
                     return false;
             }
             return true;
         }
 
-
-        public static bool CheckInRowValidate(Board board)
+        /// <summary>
+        /// this method gets the baord, if there are 2 same numbers in the same row the method returns true, false otherwise
+        /// </summary>
+        /// <param name="board"> the board in int[,] type </param>
+        /// <returns> true if there are 2 same values in the same row, and false otherwise </returns>
+        public static bool CheckInRowValidate(MatrixIntBoard board)
         {
             // this method gets the baord, if there is 2 same numbers in the same row the method returns true, false otherwise
-            for (int i = 0; i < Constants.ROWS; i++)
+            for (int i = 0; i < Constants.SIZE; i++)
             {
-                Constants.VALUESOFCELLS = Constants.AddValuesOfCells();
+                Constants.VALUES_OF_CELLS = Constants.AddValuesOfCells();
 
                 // copy the elemnts of VALUESOFCELLS to a new array called temp
-                char[] temp = new char[Constants.VALUESOFCELLS.Length];
-                Constants.VALUESOFCELLS.CopyTo(temp, 0);
+                char[] temp = new char[Constants.VALUES_OF_CELLS.Length];
+                Constants.VALUES_OF_CELLS.CopyTo(temp, 0);
 
-                for (int j = 0; j < Constants.COLS; j++)
+                for (int j = 0; j < Constants.SIZE; j++)
                 {
                     // if temp contains the board[i, j], the cell in temp will be space
-                    char value = (char)board.GetBoard()[i, j].GetValue();
+                    char value = (char)board.GetBoard()[i, j];
                     value += '0';
                     if (temp.Contains(value))
                     {
-                        temp[board.GetBoard()[i, j].GetValue() - 1] = ' ';
+                        temp[board.GetBoard()[i, j] - 1] = ' ';
                     }
                     // if not, it means that there is a value twice or more in the same row or the value is 0
                     else
                     {
-                        if (board.GetBoard()[i, j].GetValue() == 0)
+                        if (board.GetBoard()[i, j] == 0)
                             continue;
                         else
                             return false;
@@ -124,31 +106,35 @@ namespace OmegaSudoku
             return true;
         }
 
-
-        public static bool CheckInColValidate(Board board)
+        /// <summary>
+        /// this method gets the baord, if there are 2 same numbers in the same column the method returns true, false otherwise
+        /// </summary>
+        /// <param name="board"> the board in int[,] type </param>
+        /// <returns> true if there are 2 same values in the same column, and false otherwise </returns>
+        public static bool CheckInColValidate(MatrixIntBoard board)
         {
             // this method gets the baord, if there is 2 same numbers in the same col the method returns true, false otherwise
-            for (int j = 0; j < Constants.COLS; j++)
+            for (int j = 0; j < Constants.SIZE; j++)
             {
-                Constants.VALUESOFCELLS = Constants.AddValuesOfCells();
+                Constants.VALUES_OF_CELLS = Constants.AddValuesOfCells();
 
                 // copy the elemnts of VALUESOFCELLS to a new array called temp
-                char[] temp = new char[Constants.VALUESOFCELLS.Length];
-                Constants.VALUESOFCELLS.CopyTo(temp, 0);
+                char[] temp = new char[Constants.VALUES_OF_CELLS.Length];
+                Constants.VALUES_OF_CELLS.CopyTo(temp, 0);
 
-                for (int i = 0; i < Constants.ROWS; i++)
+                for (int i = 0; i < Constants.SIZE; i++)
                 {
                     // if temp contains the board[i, j], the cell in temp will be space
-                    char value = (char)board.GetBoard()[i, j].GetValue();
+                    char value = (char)board.GetBoard()[i, j];
                     value += '0';
                     if (temp.Contains(value))
                     {
-                        temp[board.GetBoard()[i, j].GetValue() - 1] = ' ';
+                        temp[board.GetBoard()[i, j] - 1] = ' ';
                     }
                     // if not, it means that there is a value twice or more in the same col or the value is 0
                     else
                     {
-                        if (board.GetBoard()[i, j].GetValue() == 0)
+                        if (board.GetBoard()[i, j] == 0)
                             continue;
                         else
                             return false;
@@ -158,24 +144,28 @@ namespace OmegaSudoku
             return true;
         }
 
-
-        public static bool CheckInSubSqueresValidate(Board board)
+        /// <summary>
+        /// this method gets the baord, if there are 2 same numbers in the same sub square the method returns true, false otherwise
+        /// </summary>
+        /// <param name="board"> the board in int[,] type </param>
+        /// <returns> true if there are 2 same values in the same sub square, and false otherwise </returns>
+        public static bool CheckInSubSqueresValidate(MatrixIntBoard board)
         {
             // this method gets the baord, if there is 2 same numbers in the same sub square the method returns true, false otherwise
 
             // squre root of ROWS and COLS
-            int sqrtRow = (int)Math.Sqrt(Constants.ROWS);
-            int sqrtCol = (int)Math.Sqrt(Constants.COLS);
+            int sqrtRow = (int)Math.Sqrt(Constants.SIZE);
+            int sqrtCol = (int)Math.Sqrt(Constants.SIZE);
 
-            for (int i = 0; i < Constants.ROWS; i += sqrtRow)
+            for (int i = 0; i < Constants.SIZE; i += sqrtRow)
             {
-                for (int j = 0; j < Constants.COLS; j += sqrtCol)
+                for (int j = 0; j < Constants.SIZE; j += sqrtCol)
                 {
-                    Constants.VALUESOFCELLS = Constants.AddValuesOfCells();
+                    Constants.VALUES_OF_CELLS = Constants.AddValuesOfCells();
 
                     // copy the elemnts of VALUESOFCELLS to a new array called temp
-                    char[] temp = new char[Constants.VALUESOFCELLS.Length];
-                    Constants.VALUESOFCELLS.CopyTo(temp, 0);
+                    char[] temp = new char[Constants.VALUES_OF_CELLS.Length];
+                    Constants.VALUES_OF_CELLS.CopyTo(temp, 0);
 
                     // loop on the sub squares in the board
                     for (int row = i; row < i + sqrtRow; row++)
@@ -183,16 +173,16 @@ namespace OmegaSudoku
                         for (int col = j; col < j + sqrtCol; col++)
                         {
                             // if temp contains the board[i, j], the cell in temp will be space
-                            char value = (char)board.GetBoard()[row, col].GetValue();
+                            char value = (char)board.GetBoard()[row, col];
                             value += '0';
                             if (temp.Contains(value))
-                            { 
-                                temp[board.GetBoard()[row, col].GetValue() - 1] = ' ';
+                            {
+                                temp[board.GetBoard()[row, col] - 1] = ' ';
                             }
                             // if not, it means that there is a value twice or more in the same sub square or the value is 0
                             else
                             {
-                                if (board.GetBoard()[row, col].GetValue() == 0)
+                                if (board.GetBoard()[row, col] == 0)
                                     continue;
                                 else
                                     return false;
